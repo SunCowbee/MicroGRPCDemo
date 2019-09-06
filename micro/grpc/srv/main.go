bc4479d269b5c37670c1d651ef4c664b0de559bd
Roger <suncowbee@163.com>
2019-09-06 17:54:33 +0800

    m

---
+++ micro/grpc/srv/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/micro/go-log"
+	"github.com/micro/go-micro"
+	"MicroGRPCDemo/micro/grpc/srv/handler"
+	example "MicroGRPCDemo/micro/grpc/srv/proto/example"
+	//01加载micro 关于grpc的插件
+	"github.com/micro/go-grpc"
+)
+
+func main() {
+	// New Service
+	//创建服务
+/*	service := micro.NewService(
+		//服务名
+		micro.Name("go.micro.srv.srv"),
+		//版本号
+		micro.Version("latest"),
+	)*/
+	//02 将之前的micro缓存grpc
+	service := grpc.NewService(
+		//服务名
+		micro.Name("go.micro.srv.srv"),
+		//版本号
+		micro.Version("latest"),
+	)
+
+	// Initialise service
+	//初始化我们的服务
+	service.Init()
+
+	// Register Handler
+	//注册我们的服务
+	example.RegisterExampleHandler(service.Server(), new(handler.Example))
+
+	// Register Struct as Subscriber
+	//注册结构体 源自于Subscriber
+	//micro.RegisterSubscriber("go.micro.srv.srv", service.Server(), new(subscriber.Example))
+
+	// Register Function as Subscriber
+	//注册一个方法 源自于Subscriber
+	//micro.RegisterSubscriber("go.micro.srv.srv", service.Server(), subscriber.Handler)
+
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ micro/grpc/srv/subscriber/example.go
@@ -0,0 +1,22 @@
+package subscriber
+
+import (
+	"context"
+	"github.com/micro/go-log"
+
+	example "MicroGRPCDemo/micro/rpc/srv/proto/example"
+)
+
+type Example struct{}
+
+func (e *Example) Handle(ctx context.Context, msg *example.Message) error {
+	//处理收到的消息
+	log.Log("Handler Received message: ", msg.Say)
+	return nil
+}
+
+func Handler(ctx context.Context, msg *example.Message) error {
+	//收到的消息函数
+	log.Log("Function Received message: ", msg.Say)
+	return nil
+}
